94520f7c105ab7a267551c4aecb849c244564039
Ananth <apadbhan.aks@gmail.com>
2020-07-01 23:17:47 +0530

    test

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,9 +4,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello World");
+		System.out.println("1Hello World");
 		System.out.println("2 No need to add any module");
-		System.out.println("Type few words & Control Space");
+		System.out.println("3Type few words & Control Space");
 	}
 
 }

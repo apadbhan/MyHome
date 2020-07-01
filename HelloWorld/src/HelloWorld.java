26abdb7528521c018feb91873277ea984b5f760d
apadbhan <42498152+apadbhan@users.noreply.github.com>
2020-07-01 22:28:14 +0530

    Update README.md
    change 2

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+		System.out.println("No need to add any module");
+		System.out.println("Type few words & Control Space");
+	}
+
+}
